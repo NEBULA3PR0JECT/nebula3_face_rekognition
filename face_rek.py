b149475b8fb5a2fca35eed72805ebd4f403c4c54
Ilan Aizelman <bebetteryou2015@gmail.com>
2023-03-08 14:20:49 +0200

    Update face_rek.py
    added more code for processing images

--- face_rek.py
+++ face_rek.py
@@ -49,53 +49,135 @@ class IMDBProcessor:
 
         return
 
+
+    def combine_pickles(self):
+        pickle_1 = self.read_data(
+            "/notebooks/nebula3_face_rekognition/representations_300000.pkl")
+        pickle_2 = self.read_data(
+            "/notebooks/nebula3_face_rekognition/representations_all.pkl")
+        for item in pickle_2:
+            pickle_1.append(item)
+        self.save_data(
+            "/notebooks/nebula3_face_rekognition/representations_combined.pkl", pickle_1)
+        print("Done")
+
+    def combine_jsons(self):
+        data_1 = self.read_data(
+            "/notebooks/nebula3_face_rekognition/actor_name_to_paths_300000.json", mode="json")
+        data_2 = self.read_data(
+            "/notebooks/nebula3_face_rekognition/actor_name_to_paths_all.json", mode="json")
+        data_3 = data_1 | data_2
+        self.save_data(
+            "/notebooks/nebula3_face_rekognition/actor_name_to_paths_combined.json", data_3, mode="json")
+
+        print("Done")
+
     def process_face(self, image_file_path, dest_file_path):
-        
+
         embedding, mtcnn_cropped_image, names, mdf_id_all, status = self.face_reid.extract_faces(path_mdf=image_file_path,
-                                                                                    result_path_good_resolution_faces=dest_file_path,
-                                                                                    plot_cropped_faces=True)
+                                                                                                 result_path_good_resolution_faces=dest_file_path,
+                                                                                                 plot_cropped_faces=False)
         if status:
-            mdf_full_path = "/".join(dest_file_path.split("/")[-2:])
-            instance = []
-            instance.append(mdf_full_path)
-            instance.append(embedding)
-            self.representations.append(instance)
+            if embedding.shape == torch.Size([1, 512]):
+                mdf_full_path = "/".join(dest_file_path.split("/")[-2:])
+                instance = []
+                instance.append(mdf_full_path)
+                instance.append(embedding)
+                self.representations.append(instance)
+            else:
+                status = False
 
         return status
 
-    def process_images(self, path):
+    def process_images_wiki_imdb(self, path):
         with open(path, "r") as f:
             reader = csv.reader(f, delimiter=',')
-            for idx, row in enumerate(reader):
-                if idx > 200000:
+            # row_count = sum(1 for row in reader)
+            for idx, row in enumerate(tqdm.tqdm(reader)):
+                # print(idx)
+                if idx > 0:
                     image_path = row[2]
                     face_conf_1 = row[3]
                     face_conf_2 = row[4]
                     if not face_conf_2 and face_conf_1 != '-inf':
-                        print("Index: {}".format(idx))
-                        actor_name = ast.literal_eval(row[5])[0]
+                        print("Index: {}/62329".format(idx))
+                        if row[5] != "[]":
+                            actor_name = ast.literal_eval(row[5])[0]
 
-                        image_full_path = os.path.join(CUR_FOLDER_PATH, image_path)
-                        dest_file_path = os.path.join(CUR_FOLDER_PATH, image_path.replace("imdb_crop", DEST_FOLDER_NAME))
-                        dest_image_path = "/".join(dest_file_path.split("/")[:-1])
-                        os.makedirs(dest_image_path, exist_ok=True)
-                        face_found = self.process_face(image_full_path, dest_file_path)
-                        if face_found:
-                            image_path = "/".join(image_path.split("/")[-2:])
-                            if actor_name not in self.actor_name_to_paths:
-                                self.actor_name_to_paths[actor_name] = [image_path]
-                            else:
-                                self.actor_name_to_paths[actor_name].append(image_path)
+                            image_full_path = os.path.join(
+                                CUR_FOLDER_PATH, image_path)
+                            dest_file_path = os.path.join(
+                                CUR_FOLDER_PATH, image_path.replace("wiki_crop", DEST_FOLDER_NAME))
+                            dest_image_path = "/".join(
+                                dest_file_path.split("/")[:-1])
+                            os.makedirs(dest_image_path, exist_ok=True)
+                            face_found = self.process_face(
+                                image_full_path, dest_file_path)
+                            if face_found:
+                                image_path = "/".join(image_path.split("/")
+                                                      [-2:])
+                                if actor_name not in self.actor_name_to_paths:
+                                    self.actor_name_to_paths[actor_name] = [
+                                        image_path]
+                                else:
+                                    self.actor_name_to_paths[actor_name].append(
+                                        image_path)
 
-                    if idx % 10000 == 0:
+                    if idx % 100000 == 0:
                         print("Saved: {}".format(idx))
-                        path = os.path.join(CUR_FOLDER_PATH, "actor_name_to_paths_part2.json")
-                        if self.actor_name_to_paths:        
-                            self.save_data(path, data=self.actor_name_to_paths, mode="json")
-                        path = os.path.join(CUR_FOLDER_PATH, "representations_part2.pkl")
+                        path = os.path.join(
+                            CUR_FOLDER_PATH, "actor_name_to_paths_wiki_face_{}.json".format(idx))
+                        if self.actor_name_to_paths:
+                            self.save_data(
+                                path, data=self.actor_name_to_paths, mode="json")
+                        path = os.path.join(
+                            CUR_FOLDER_PATH, "representations_wiki_face_{}.pkl".format(idx))
                         if self.representations:
-                            self.save_data(path, data=self.representations, mode="pickle")
+                            self.save_data(
+                                path, data=self.representations, mode="pickle")
                         print("Done.")
+                        self.representations = list()
+                        self.actor_name_to_paths = dict()
+
+        path = os.path.join(
+            CUR_FOLDER_PATH, "actor_name_to_paths_wiki_all.json")
+        if self.actor_name_to_paths:
+            self.save_data(path, data=self.actor_name_to_paths, mode="json")
+        path = os.path.join(CUR_FOLDER_PATH, "representations_wiki_all.pkl")
+        if self.representations:
+            self.save_data(path, data=self.representations, mode="pickle")
+        print("Done.")
+
+    # def process_images_imdb_face(self, path):
+    #     with open(path, "r") as f:
+    #         reader = csv.reader(f, delimiter=',')
+    #         for idx, row in enumerate(tqdm.tqdm(reader)):
+    #             # print(idx)
+    #             if idx > 200000:
+    #                 if idx % 1000 == 0:
+    #                     print("Current Index: {}".format(idx))
+    #                 image_url = row[5]
+    #                 actor_name = row[0]
+
+    #                 face_found = self.process_face(image_url, image_url)
+    #                 if face_found:
+    #                     image_path = "/".join(image_url.split("/")[-2:])
+    #                     if actor_name not in self.actor_name_to_paths:
+    #                         self.actor_name_to_paths[actor_name] = [image_path]
+    #                     else:
+    #                         self.actor_name_to_paths[actor_name].append(image_path)
+
+    #                 if idx % 100000 == 0:
+    #                     print("Saved: {}".format(idx))
+    #                     path = os.path.join(CUR_FOLDER_PATH, "actor_name_to_paths_{}.json".format(idx))
+    #                     if self.actor_name_to_paths:
+    #                         self.save_data(path, data=self.actor_name_to_paths, mode="json")
+    #                     path = os.path.join(CUR_FOLDER_PATH, "representations_{}.pkl".format(idx))
+    #                     if self.representations:
+    #                         self.save_data(path, data=self.representations, mode="pickle")
+    #                     print("Done.")
+    #                     self.representations = list()
+    #                     self.actor_name_to_paths = dict()
 
     def save_data(self, path, data, mode="pickle"):
         if mode == "pickle":
@@ -109,7 +191,6 @@ class IMDBProcessor:
         elif mode == "json":
             with open(path, "w") as f:
                 json.dump(data, f)
-
 
     def read_data(self, path, mode="pickle"):
         if mode == "pickle":
@@ -125,19 +206,25 @@ class IMDBProcessor:
 
 def main():
     imdb_processor = IMDBProcessor()
-    imdb_path = "/notebooks/imdb_data.csv"
+
+    # imdb_processor.combine_pickles()
+    # imdb_processor.combine_jsons()
+    # imdb_path = "/notebooks/nebula3_face_rekognition/IMDb-Face.csv"
+    # imdb_path = "/notebooks/wiki.csv"
     # imdb_data = imdb_processor.build_imdb_data(imdb_path)
     # images_path = "/notebooks/nebula3_face_rekognition/imdb_crop"
-    # imdb_processor.actor_name_to_paths = imdb_processor.read_data("/notebooks/nebula3_face_rekognition/actor_name_to_paths_copy.json", mode="json")
-    # imdb_processor.representations = imdb_processor.read_data("/notebooks/nebula3_face_rekognition/representations.pkl", mode="pickle")
-    # path = os.path.join(CUR_FOLDER_PATH, "actor_name_to_cluster.pkl")
-    # imdb_processor.compute_clusters(path) 
-    imdb_processor.process_images(imdb_path)
+    imdb_processor.actor_name_to_paths = imdb_processor.read_data(
+        "/notebooks/nebula3_face_rekognition/actor_name_to_paths_all.json", mode="json")
+    imdb_processor.representations = imdb_processor.read_data(
+        "/notebooks/nebula3_face_rekognition/representations_all.pkl", mode="pickle")
+    path = os.path.join(CUR_FOLDER_PATH, "actor_name_to_cluster_100.pkl")
+    imdb_processor.compute_clusters(
+        path, distance_type='cos', radius_type='mean')
+    # imdb_processor.process_images_wiki_imdb(imdb_path)
 
 
 if __name__ == '__main__':
-    main()
-
+    # main()
 
 
 
